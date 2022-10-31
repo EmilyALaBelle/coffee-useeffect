d88135831e7e157778cf03ce6e5a50c6ca85c03f
Emily LaBelle <emilylabelle8@gmail.com>
2022-10-31 14:34:58 -0400

    first

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   const getCoffee = () => {
-    fetch()
-      .then(() => {})
-      .then(() => {})
+    fetch('https://api.sampleapis.com/coffee/hot')
+      .then(responce => responce.json())
+      .then(data => console.log(data))
       .catch((err) => {console.error(err)})
 
   }
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>My Coffees App</h1>
+        <button onClick={() => getCoffee()}>Get Me Coffee</button>
       </header>
     </div>
   );
