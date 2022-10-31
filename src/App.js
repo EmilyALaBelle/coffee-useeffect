8241621dd9fc9a25830531d7e1513322b134bdb7
Emily LaBelle <emilylabelle8@gmail.com>
2022-10-31 14:21:04 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getCoffee = () => {
+    fetch()
+      .then(() => {})
+      .then(() => {})
+      .catch((err) => {console.error(err)})
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Coffees App</h1>
       </header>
     </div>
   );
